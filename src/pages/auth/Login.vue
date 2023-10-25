8621aba5722ac70019321d94a53e726917c7e0c9
danigracia-ion <146746314+danigracia-itb@users.noreply.github.com>
2023-10-25 16:06:52 +0200

    grid for login

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="container-auth">
-        <div class="imagen-izquierda">
+    <div class="row">
+        <div class="col-5 d-none d-md-block bg-primary">
             <img src="tu-imagen.jpg" alt="Descripción de la imagen">
         </div>
-        <div class="contenido-derecha">
+        <div class="col-md-7 d-block">
+            <h1>Login</h1>
         </div>
     </div>
 </template>
@@ -11,26 +12,3 @@
 <script setup>
 
 </script>
-
-<style scoped>
-.container-auth {
-    display: grid;
-    grid-template-columns: 3fr 2fr;
-}
-
-.imagen-izquierda {
-    flex: 1;
-}
-
-.imagen-izquierda img {
-    max-width: 100%;
-    height: auto;
-    display: block;
-}
-
-.contenido-derecha {
-    flex: 2;
-    padding: 20px;
-}
-
-</style>
