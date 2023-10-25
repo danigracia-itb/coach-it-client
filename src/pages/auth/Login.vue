c8aec37498d72bc0a772a20ca0fa8ecd8f8f0edb
adrian <adrian.sanchez.7e6@itb.cat>
2023-10-25 16:52:36 +0200

    login styles

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,18 +1,44 @@
 <template>
     <div class="row">
-        <div  class="img-auth vh-100 col-5 d-none d-md-block">
-            
+        <div class="img-auth vh-100 col-5 d-none d-md-block">
+
         </div>
-        <div class="col-md-7 d-block">
-            <img class="logo-" src="../../assets/logo.png" alt="">
+        <div class="col-md-7 d-flex flex-column align-items-center justify-content-center">
+            <img class="auth-logo-img" src="../../assets/logo.png" alt="">
+            <div class="border rounded p-5">
+                <h1 class="text-uppercase text-center fw-bold">log in</h1>
+                <form class="text-center">
+                    <div class="form-group mt-5">
+                        <label for="email" class="text-uppercase fw-bold">Email</label>
+                        <input type="email" id="email" class="form-control" placeholder="Introduce your email">
+                    </div>
+
+                    <div class="form-group mt-5">
+                        <label for="password" class="text-uppercase fw-bold">Password</label>
+                        <input type="password" id="password" class="form-control" placeholder="Introduce your password">
+                    </div>
+
+                    <button type="submit" class="text-uppercase btn btn-primary mt-5">Log in</button>
+
+                </form>
+                <div class="mt-5">
+                    <p>Don't you have account? Create one <br>
+                        <RouterLink to="/register?type=coach" class="text-primary">Coach</RouterLink> or <RouterLink
+                            to="/register?type=coach" class="text-primary">Athlete</RouterLink>
+                    </p>
+                    <p>Forgot password? <RouterLink to="/request-password-recover" class="text-primary">Recover it
+                        </RouterLink>
+                    </p>
+
+                </div>
+            </div>
+            <RouterLink to="/" class="mt-5">Home</RouterLink>
         </div>
     </div>
 </template>
 
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
