2c476f92ed8dcca00de72e58d0f68aede5852551
adrian <adrian.sanchez.7e6@itb.cat>
2023-10-30 16:33:22 +0100

    diseño login modificado

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,36 +1,34 @@
 <template>
     <div class="row">
-        <div class="img-auth vh-100 col-7 d-none d-md-block" src="recover.png">
+        <div class="img-auth vh-100 col-7 d-none d-md-block">
 
         </div>
         <div class="col-md-5 d-flex flex-column align-items-center justify-content-center">
-            <img class="auth-logo-img" src="../../assets/logo.png" alt="">
-            <div class="p-8 border rounded-5">
+           <RouterLink to="/"><img class="auth-logo-img" src="../../assets/logo.png" alt=""></RouterLink>
+            <div class="p-5 px-5 border rounded-5">
                 <h1 class="text-uppercase text-center fw-bold">login</h1>
-                <form @submit.prevent="login" class="">
+                <form>
                     <div class="form-group mt-5">
                         <label for="email">Email</label>
-                        <input v-model="email" type="email" id="email" class="form-control p-3" placeholder="Introduce your email">
+                        <input type="email" id="email" class="form-control p-3" placeholder="Introduce your email">
                     </div>
 
                     <div class="form-group mt-5">
                         <label for="password">Password</label>
-                        <input v-model="password" type="password" id="password" class="form-control p-3" placeholder="Introduce your password">
+                        <input type="password" id="password" class="form-control p-3" placeholder="Introduce your password">
                     </div>
                     <button type="submit" class="text-uppercase btn btn-primary mt-5 rounded-4 w-100">Login</button>
                 </form>
                 <div class="mt-5 text-small">
-                    <p class="text-lg">Don't you have account? Create one <br>
+                    <p class="text-lg">Don't you have account? <br>Create one 
                         <RouterLink to="/register?type=coach" class="text-primary fw-bold">Coach</RouterLink> or <RouterLink
                             to="/register?type=athlete" class="text-primary fw-bold">Athlete</RouterLink>
                     </p>
                     <p>Forgot password? <RouterLink to="/request-password-recover" class="text-primary fw-bold">Recover it
                         </RouterLink>
                     </p>
-
                 </div>
             </div>
-            <RouterLink to="/" class="mt-5">Home</RouterLink>
         </div>
     </div>
 </template>
@@ -91,10 +89,4 @@ const login = async (e) => {
 // const Spinner = defineAsyncComponent(() => import("../components/layout/Spinner.vue"));
 </script>
 
-<style scoped>
-.p-8 {
-    padding: 7rem;
-}
-</style>
-
-
+<style scoped></style>
