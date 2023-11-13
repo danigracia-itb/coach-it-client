fd0c8c38d26a38f6fcbe83421cb7abe17c3ad7d6
nerea <nerea.tirado.7e6@itb.cat>
2023-11-13 16:51:03 +0100

    botones de pasar de pagina de formulario

--- src/pages/auth/Form.vue
+++ src/pages/auth/Form.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="p-3 mb-2 bg-form text-white full-screen d-flex justify-content-center align-items-center">
-        <div class="p-5 px-5 border rounded-5 bg-white text-center">
+        <form class="p-5 px-5 border rounded-5 bg-white text-center">
             <img class="auth-logo-img mx-auto" src="../../assets/logo.png" alt="">
-            <form class="text-center">
+
+            <fieldset v-if="currentPage === 1" class="text-center">
                 <div class="form-group mt-5">
                     <label for="date" class="text-black">Date of Birth</label>
                     <input type="date" id="date" class="form-control" placeholder="Introduce your birthday">
@@ -17,10 +18,11 @@
                     <label for="bodyWeight" class="text-black">Body weight</label>
                     <input type="text" id="bodyWeight" class="form-control" placeholder="Introduce your body weight">
                 </div>
-            </form>
+                <button @click="nextPage">Next</button>
+            </fieldset>
 
 
-            <form class="text-center">
+            <fieldset v-if="currentPage === 2" class="text-center">
                 <div class="form-group mt-5">
                     <label for="timeTraining" class="text-black">Time training:</label>
                     <select id="timeTraining" class="form-control">
@@ -38,64 +40,65 @@
 
                 <div class="form-group mt-5 text-black">
                     <label class="text-black">Days available for training</label><br>
-                    <input type="checkbox" id="monday" name="day" value="Monday">
-                    <label for="monday">Mon</label><br>
-                    <input type="checkbox" id="tuesday" name="day" value="Tuesday">
-                    <label for="tuesday">Tuesday</label><br>
-                    <input type="checkbox" id="wednesday" name="day" value="Wednesday">
-                    <label for="wednesday">Wednesday</label><br>
-                    <input type="checkbox" id="thursday" name="day" value="Thursday">
-                    <label for="thursday">Thursday</label><br>
-                    <input type="checkbox" id="friday" name="day" value="Friday">
-                    <label for="friday">Friday</label><br>
-                    <input type="checkbox" id="saturday" name="day" value="Saturday">
-                    <label for="saturday">Saturday</label><br>
-                    <input type="checkbox" id="sunday" name="day" value="Sunday">
-                    <label for="sunday">Sunday</label><br>
+                    <span><input type="checkbox" id="monday" name="day" value="Monday"><label
+                            for="monday">Mon</label></span>
+                    <span><input type="checkbox" id="tuesday" name="day" value="Tuesday"><label
+                            for="tuesday">Tue</label></span>
+                    <span><input type="checkbox" id="wednesday" name="day" value="Wednesday"><label
+                            for="wednesday">Wed</label></span>
+                    <span><input type="checkbox" id="thursday" name="day" value="Thursday"><label
+                            for="thursday">Thu</label></span>
+                    <span><input type="checkbox" id="friday" name="day" value="Friday"><label
+                            for="friday">Fri</label></span>
+                    <span><input type="checkbox" id="saturday" name="day" value="Saturday"><label
+                            for="saturday">Sat</label></span>
+                    <span><input type="checkbox" id="sunday" name="day" value="Sunday"><label
+                            for="sunday">Sun</label></span>
+                </div>
+                <button @click="nextPage">Next</button>
+            </fieldset>
+
+            <fieldset v-if="currentPage === 3" class="text-center">
+                <div class="form-group">
+                    <label for="favoriteExercises" class="text-black">Favorite Exercises</label>
+                    <textarea id="favoriteExercises" class="form-control"
+                        placeholder="Enter your favorite exercises"></textarea>
                 </div>
 
-            </form>
+                <div class="form-group">
+                    <label for="hatedExercises" class="text-black">Hated Exercises</label>
+                    <textarea id="hatedExercises" class="form-control" placeholder="Enter your hated exercises"></textarea>
+                </div>
+                <button @click="nextPage">Next</button>
+            </fieldset>
 
-            <form class="text-center">
-                <div class="form-group mt-5">
-                    <label for="age" class="text-black">How old are you?</label>
-                    <input type="age" id="age" class="form-control" placeholder="Introduce your age">
+            <fieldset v-if="currentPage === 4" class="text-center">
+                <div class="form-group">
+                    <label for="shortTermGoals" class="text-black">Short-Term Goals</label>
+                    <textarea id="shortTermGoals" class="form-control" placeholder="Enter your short-term goals"></textarea>
                 </div>
 
-                <div class="form-group mt-5">
-                    <label for="weight" class="text-black">Introduce your weight</label>
-                    <input type="weight" id="weight" class="form-control" placeholder="Introduce your weight">
+                <div class="form-group">
+                    <label for="longTermGoals" class="text-black">Long-Term Goals</label>
+                    <textarea id="longTermGoals" class="form-control" placeholder="Enter your long-term goals"></textarea>
                 </div>
-
-                <div class="form-group mt-5">
-                    <label for="height" class="text-black">What's your height?</label>
-                    <input type="height" id="height" class="form-control" placeholder="Introduce your height">
-                </div>
-            </form>
-
-            <form class="text-center">
-                <div class="form-group mt-5">
-                    <label for="age" class="text-black">How old are you?</label>
-                    <input type="age" id="age" class="form-control" placeholder="Introduce your age">
-                </div>
-
-                <div class="form-group mt-5">
-                    <label for="weight" class="text-black">Introduce your weight</label>
-                    <input type="weight" id="weight" class="form-control" placeholder="Introduce your weight">
-                </div>
-
-                <div class="form-group mt-5">
-                    <label for="height" class="text-black">What's your height?</label>
-                    <input type="height" id="height" class="form-control" placeholder="Introduce your height">
-                </div>
-            </form>
-        </div>
+                <button @click="nextPage">Next</button>
+            </fieldset>
+        </form>
     </div>
 </template>
   
-
 <script setup>
+import { ref } from 'vue';
 
+const currentPage = ref(1);
+const totalPages = 4;
+
+function nextPage() {
+    if (currentPage.value < totalPages) {
+        currentPage.value += 1;
+    }
+}
 </script>
 
 <style scoped>
@@ -114,5 +117,17 @@
     background-image: url(../../assets/img/img-form.jpg);
     background-repeat: no-repeat;
     background-size: cover;
-} 
+}
+
+span {
+    margin: 1%;
+}
+
+label {
+    margin-left: 1%;
+}
+button{
+    background-color: #711bba;
+    margin-top: 5%;
+}
 </style>
