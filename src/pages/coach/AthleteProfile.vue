0022a7ce14d371f48e24aee62e648c6bd72a9b61
adrian <adrian.sanchez.7e6@itb.cat>
2023-11-23 15:30:42 +0100

    athlete profile diseño acabado falta responsive

--- src/pages/coach/AthleteProfile.vue
+++ src/pages/coach/AthleteProfile.vue
@@ -3,74 +3,84 @@
         <Spinner v-if="loading" />
 
         <div v-else>
-            <h1 class="text-center">{{ athlete.name }}</h1>
+            <h1 class="text-center m-5">{{ athlete.name }}</h1>
             <div class="row">
-                <div class="col-5 bg-primary">
-                    <p>izquierda</p>
+                <div class="col-lg-4 mb-4 mb-lg-0 d-flex justify-content-center align-items-center">
+                    <img class="rounded-circle" width="400" :src="athlete.picture" alt="">
                 </div>
-                <div class="col-7 bg-danger">
-                    <div class="d-flex align-items-center">
-                        <font-awesome-icon class="p-3" icon="fa-solid fa-envelope" size="2xl" />
-                        <p class="m-0">{{ athlete.email }}</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <font-awesome-icon class="p-3" icon="fa-solid fa-cake-candles" size="2xl" />
-                        <p class="m-0">22/11/2023</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <font-awesome-icon class="p-3" icon="fa-solid fa-ruler-vertical" size="2xl" />
-                        <p class="m-0">170cm</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <font-awesome-icon class="p-3" icon="fa-solid fa-weight-scale" size="2xl" />
-                        <font-awesome-icon class="p-3" icon="fa-solid fa-weight-scale" size="2xl"
-                            :style="{ color: 'green' }" />
-
-                        <p class="m-0">70kg</p>
-                    </div>
-
-                    <p><strong>Time you have been training:</strong></p>
-                    <p><strong>Time available for training:</strong></p>
 
 
-                    <table class="table table-bordered text-center">
-                        <thead>
-                            <tr>
-                                <th>Monday</th>
-                                <th>Tuesday</th>
-                                <th>Wednesday</th>
-                                <th>Thursday</th>
-                                <th>Friday</th>
-                                <th>Saturday</th>
-                                <th>Sunday</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl"/></td>
-                            <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl"/></td>
+                <div class="col-lg-8">
+                    <div class="mb-4">
+                        <div class="d-flex align-items-center">
+                            <font-awesome-icon class="p-3" icon="fa-solid fa-envelope" size="2xl" />
+                            <p class="m-0">{{ athlete.email }}</p>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <font-awesome-icon class="p-3" icon="fa-solid fa-cake-candles" size="2xl" />
+                            <p class="m-0">22/11/2023</p>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <font-awesome-icon class="p-3" icon="fa-solid fa-ruler-vertical" size="2xl" />
+                            <p class="m-0">170cm</p>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <font-awesome-icon class="p-3" icon="fa-solid fa-weight-scale" size="2xl" />
+                            <p class="m-0">70kg</p>
+                        </div>
 
-                            <!-- <td>
+                        <p><strong>Time training:</strong></p>
+                        <p><strong>Time for training:</strong></p>
+
+
+                        <table class="table table-bordered text-center">
+                            <thead>
+                                <tr>
+                                    <th>Monday</th>
+                                    <th>Tuesday</th>
+                                    <th>Wednesday</th>
+                                    <th>Thursday</th>
+                                    <th>Friday</th>
+                                    <th>Saturday</th>
+                                    <th>Sunday</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-xmark" class="text-danger" size="xl" />
+                                    </td>
+                                    <td><font-awesome-icon icon="fa-solid fa-square-check" class="text-success" size="xl" />
+                                    </td>
+                                    <!-- <td>
                                 {{ athlete.available_days.monday ? 'true' :'false' }}
-                            </td> -->
-                        </tr>
-                        </tbody>
+                                </td> -->
+                                </tr>
+                            </tbody>
 
-                    </table>
+                        </table>
+
+                        <div class="d-flex align-items-center">
+                            <font-awesome-icon class="p-3" icon="fa-solid fa-star" size="2xl" />
+                            <p class="m-0"><strong>Favorite exercises: </strong></p>
+                        </div>
+
+                        <p><strong>Hated exercises:</strong></p>
+                        <p><strong>Short-term goals:</strong></p>
+                        <p><strong>Long-term goals:</strong></p>
+
+                    </div>
                 </div>
             </div>
-
-            <img class="" width="" :src="athlete.picture" alt="">
-
-
-
-
-
         </div>
     </div>
 </template>
