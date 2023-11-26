17b50fdf8b8ef16b07006f8bf46446c65b0812a5
sergio.gonzalez.7e6 <sergio.gonzalez.7e6@itb.cat>
2023-11-26 14:16:13 +0100

    Push main home

---
+++ src/components/CookieBanner.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="!cookiesAccepted" class="banner">
+        <p>
+            We use tracking cookies to understand how you use the product and help us improve it.
+            Please accept cookies to help us improve.
+        </p>
+        <button class="btn btn-primary" type="button" @click="acceptCookies">Accept cookies</button>
+        <button class="btn btn-primary" type="button" @click="declineCookies">Decline cookies</button>
+    </div>
+</template>
+
+<!--<script>
+export default {
+    data() {
+        return {
+            cookiesAccepted: false,
+        };
+    },
+    mounted() {
+        const cookiesAccepted = localStorage.getItem('cookiesAccepted');
+        if (cookiesAccepted) {
+            this.cookiesAccepted = true;
+        }
+    },
+    methods: {
+        acceptCookies() {
+            this.cookiesAccepted = true;
+            localStorage.setItem('cookiesAccepted', false);
+        },
+        declineCookies() {
+            this.cookiesAccepted = true;
+            localStorage.setItem('cookiesAccepted', false);
+            console.log(clearLoc)
+        },
+        handleBeforeUnload() {
+            // Borrar el valor de localStorage al reiniciar la página
+            localStorage.removeItem('cookiesAccepted');
+        },
+    },
+};
+</script>-->
+
+<style scoped>
+.banner {
+    position: fixed;
+    z-index: 999;
+    background-color: #dbdbdbde;
+    padding: 20px;
+    text-align: center;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.btn {
+    margin-left: 20px;
+}
+</style>
