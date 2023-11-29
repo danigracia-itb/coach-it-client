e1a0c26d266790aa39268e07513779337431b72d
Sergio <sergio.gonzalez.7e6@itb.cat>
2023-11-29 16:58:01 +0100

    Privacy Policy

--- src/pages/public/PrivacyPolicy.vue
+++ src/pages/public/PrivacyPolicy.vue
@@ -1,4 +1,82 @@
 <template>
+    <header>
+        <section class="bg-dark d-flex">
+            <a class="social" href="https://www.instagram.com">
+                <img
+                    class="social-net svg-container"
+                    src="/src/assets/img/instagram.svg"
+                />
+            </a>
+            <a class="social" href="https://www.facebook.com">
+                <img class="social-net" src="/src/assets/img/facebook.svg" />
+            </a>
+            <a class="social" href="https://www.twitter.com">
+                <img class="social-net" src="/src/assets/img/twitterX.svg" />
+            </a>
+        </section>
+
+        <section
+            class="bg-light px-5 py-2 d-flex flex-column flex-sm-row justify-content-between align-items-center shadow-sm"
+        >
+            <RouterLink to="/"
+                ><img width="300" src="../../assets/logo.png"
+            /></RouterLink>
+            <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+                <div class="container-fluid">
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        @click="toggleNav"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        :aria-expanded="isNavOpen ? 'true' : 'false'"
+                        aria-label="Toggle navigation"
+                        style="font-size: 4rem"
+                    >
+                        >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        :class="[
+                            'collapse',
+                            'navbar-collapse',
+                            { show: isNavOpen },
+                        ]"
+                        id="navbarNav"
+                    >
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link home fw-bold" href="#"
+                                    >Home</a
+                                >
+                            </li>
+                            <li class="nav-item">
+                                <RouterLink to="/aboutus"
+                                    ><a class="nav-link" href="#"
+                                        >About us</a
+                                    ></RouterLink
+                                >
+                            </li>
+                            <li class="nav-item">
+                                <RouterLink to="/login"
+                                    ><a class="nav-link" href="#"
+                                        >Login</a
+                                    ></RouterLink
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                    <RouterLink to="/register"
+                        ><button class="btn start-today-btn btn btn-primary">
+                            Start Today
+                        </button></RouterLink
+                    >
+                </div>
+            </nav>
+        </section>
+    </header>
+    <img src="/src/assets/img/privacy-policy.jpg" />
     <div>
         <h2>Privacy Policy</h2>
 
@@ -58,3 +136,91 @@
         </p>
     </div>
 </template>
+
+<style>
+.bg-dark {
+    background-color: #000;
+    padding: 15px;
+}
+
+.d-flex {
+    display: flex;
+    justify-content: flex-end;
+}
+
+svg {
+    color: #ffffff;
+}
+
+.social {
+    margin-left: 15px;
+    transition: color 0.3s ease;
+}
+
+.social:hover {
+    color: #9329f4;
+}
+
+.social-net {
+    width: 30px;
+    height: auto;
+    fill: #ffffff;
+    transition: fill 0.3s ease;
+}
+
+.social-net:hover {
+    fill: #9329f4;
+}
+
+.social-net:hover {
+    filter: brightness(1.2);
+}
+
+.navbar-custom .navbar-nav .nav-link {
+    color: #000000;
+    margin-right: 20px;
+}
+
+.navbar-nav .nav-link:hover {
+    color: #9329f4;
+    text-decoration-thickness: 2px;
+}
+
+.nav-link {
+    text-decoration: none;
+    padding: 5px;
+    display: inline-block;
+    text-decoration: none;
+    padding: 5px;
+    display: inline-block;
+    position: relative;
+}
+
+.nav-link::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: transparent;
+    transition: background-color 0.3s ease;
+}
+
+.nav-link:hover::after {
+    background-color: #9329f4;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: transparent;
+    transition: background-color 0.3s ease;
+}
+
+.nav-link:hover::after {
+    background-color: #9329f4;
+}
+
+.home {
+    pointer-events: none;
+}
+</style>
