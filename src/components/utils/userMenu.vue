3871f2218cc1419018ac4b4523343663f0856b2f
nerea <nerea@ubuntu.myguest.virtualbox.org>
2024-01-17 15:12:51 +0100

    user menu

--- src/components/utils/userMenu.vue
+++ src/components/utils/userMenu.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <RouterLink to="" class="text-center border-bottom bg-primary">
-            <p class="mt-4 fs-1">EDIT PROFILE</p>
+        <RouterLink to="/coach/profile" class="text-center border-bottom bg-primary">
+            <p class="mt-4 fs-1"><font-awesome-icon icon="fa-solid fa-user-pen" /> EDIT PROFILE</p>
         </RouterLink>
-        <button>LOG OUT</button>
+        <button @click="logOut()">LOG OUT</button>
     </div>
 </template>
+
+<script setup>
+</script>
