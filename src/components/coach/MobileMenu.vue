c299ec33884ddc0a81010df711f3893e5fd6914e
nerea <nerea@ubuntu.myguest.virtualbox.org>
2024-01-25 16:53:17 +0100

    menu mobil

---
+++ src/components/coach/MobileMenu.vue
@@ -0,0 +1,68 @@
+<template>
+            <div v-if="showMenu" id="myNav" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" @click="toggleMenu">&times;</a>
+        <div class="overlay-content">
+            <RouterLink to="/coach">Dashboard</RouterLink>
+            <RouterLink to="/coach/exercises">Exercises</RouterLink>
+            <hr class="bg-white border-5 border-top border-white">
+            <RouterLink to="/coach/profile">Edit profile</RouterLink>
+            <RouterLink to="/coach/exercises" class="text-danger fw-bold d-flex gap-3 justify-content-center align-items-center"><font-awesome-icon icon="fa-solid fa-right-from-bracket"/><span>Log out</span></RouterLink>
+        </div>
+        </div>
+</template>
+
+<script setup>
+const {showMenu, toggleMenu} = defineProps(["showMenu", "toggleMenu"])
+</script>
+
+<style scoped>
+.overlay {
+    width: 100%;
+    height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0, 0.9);
+  overflow-x: hidden;
+  transition: 0.5s;
+}
+
+.overlay-content {
+  position: relative;
+  top: 25%;
+  width: 100%;
+  text-align: center;
+  margin-top: 30px;
+}
+
+.overlay a {
+  padding: 8px;
+  text-decoration: none;
+  font-size: 36px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.overlay a:hover, .overlay a:focus {
+  color: #f1f1f1;
+}
+
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+}
+
+@media screen and (max-height: 450px) {
+  .overlay a {font-size: 20px}
+  .overlay .closebtn {
+  font-size: 40px;
+  top: 15px;
+  right: 35px;
+  }
+}
+</style>
--- src/components/utils/userMenu.vue
+++ src/components/utils/userMenu.vue
@@ -7,22 +7,6 @@
             alt=""
         />
     </button>
-    <!-- <div class="text-center bg-light">
-        <RouterLink
-            to="/coach/profile"
-            class="text-center border-bottom bg-primary"
-        >
-            <p class="mt-4 fs-1">
-                <font-awesome-icon icon="fa-solid fa-user-pen" /> EDIT PROFILE
-            </p>
-        </RouterLink>
-        <button
-            class="bg-danger text-white m-3 mb-4 border-0 p-3"
-            @click="logOut(router)"
-        >
-            LOG OUT
-        </button>
-    </div> -->
 </template>
 
 <script setup>
@@ -73,9 +57,7 @@ function onContextMenu(e) {
                 }),
 
                 onClick: () => {
-                    router.push({
-                        path: "/coach/profile",
-                    });
+                    logOut(router)
                 },
             },
         ],
