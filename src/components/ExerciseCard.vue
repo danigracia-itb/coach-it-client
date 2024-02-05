d1e13ffd4720f3b61b7654139dc9c3b8779f03e9
nerea <nerea.tirado.7e6@itb.cat>
2024-02-05 18:20:31 +0100

    tooltips

--- src/components/ExerciseCard.vue
+++ src/components/ExerciseCard.vue
@@ -1,42 +1,53 @@
 <template>
-    <div
-        class="card"
-        :class="exercise.is_default ? 'bg-light' : 'bg-primary text-white card-nodefault'"
-    >
-        <div
-            class="p-3 d-flex flex-row justify-content-between align-items-center"
-        >
-            <p class="fw-bold mb-0">{{ exercise.name }}</p>
-            <a target="_blank" :href="`https://www.google.com/search?tbm=isch&q=${exercise.name}`"><font-awesome-icon icon="fa-solid fa-eye" /></a>
-            <button class="delete-btn btn btn-danger" v-if="!exercise.is_default" @click="() => deleteExercise(exercise.id)">
-                <font-awesome-icon icon="fa-solid fa-trash" />
-            </button>
-            <p class="mb-0" v-else>Default</p>
-        </div>
+  <div
+    class="card"
+    :class="
+      exercise.is_default ? 'bg-light' : 'bg-primary text-white card-nodefault'
+    "
+  >
+    <div class="p-3 d-flex flex-row justify-content-between align-items-center">
+      <a
+        target="_blank"
+        :href="`https://www.google.com/search?tbm=isch&q=${exercise.name}`"
+        ><font-awesome-icon icon="fa-solid fa-eye"
+      /></a>
+      <p class="fw-bold mb-0">{{ exercise.name }}</p>
+
+      <button
+        class="delete-btn btn btn-danger"
+        v-if="!exercise.is_default"
+        @click="() => deleteExercise(exercise.id)"
+      >
+        <font-awesome-icon icon="fa-solid fa-trash" />
+      </button>
+      <p class="mb-0" v-else>Default</p>
     </div>
+  </div>
 </template>
 
 <script setup>
-import axiosClient from '../config/axios';
-const { exercise, deleteExercise } = defineProps(["exercise", "deleteExercise"]);
-
+import axiosClient from "../config/axios";
+const { exercise, deleteExercise } = defineProps([
+  "exercise",
+  "deleteExercise",
+]);
 </script>
 
 <style>
 .card-nodefault {
-    transition: all ease-in-out .4s;
+  transition: all ease-in-out 0.4s;
 }
 .card-nodefault:hover {
-    background-color: black !important;
-    transition: all ease-in-out .4s;
+  background-color: black !important;
+  transition: all ease-in-out 0.4s;
 }
 
 .card-nodefault:hover .delete-btn {
-    opacity: 1;
-    transition: all ease-in-out .4s;
+  opacity: 1;
+  transition: all ease-in-out 0.4s;
 }
 .delete-btn {
-    opacity: 0;
-    transition: all ease-in-out .4s;
+  opacity: 0;
+  transition: all ease-in-out 0.4s;
 }
 </style>
--- src/pages/coach/Exercises.vue
+++ src/pages/coach/Exercises.vue
@@ -1,128 +1,144 @@
 <template>
     <div>
-        <h1 class="text-center mt-5">Exercises</h1>
-
-        <!-- Acciones -->
-        <div class="d-flex justify-content-end">
-            <button class="btn btn-primary" @click="addExercise">
-                <font-awesome-icon icon="fa-solid fa-plus" />
-            </button>
+      <h1 class="text-center mt-5">Exercises</h1>
+  
+      <!-- Acciones -->
+      <div class="d-flex justify-content-end">
+        <button class="btn btn-primary" @click="addExercise">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+        </button>
+      </div>
+      <Spinner class="mt-5" v-if="loading" />
+  
+      <div v-else>
+        <div
+          v-for="(exercises, group) in groupedExercises"
+          :key="group"
+          class="mt-5"
+        >
+          <h4>
+            {{ group }}<font-awesome-icon v-tooltip="getTooltipContent(group)" icon="fa-regular fa-circle-question" class="fa-xs pr-2"/>
+          </h4>
+          <div class="exercises-grid">
+            <ExerciseCard
+              v-for="exercise in exercises"
+              :key="exercise.id"
+              :exercise="exercise"
+              :deleteExercise="deleteExercise"
+            />
+          </div>
         </div>
-        <Spinner class="mt-5" v-if="loading" />
-
-        <div v-else>
-            <div v-for="(exercises, group) in groupedExercises" :key="group" class="mt-5">
-                <h4>{{ group }}</h4>
-
-                <div class="exercises-grid">
-                    <ExerciseCard
-                        v-for="exercise in exercises"
-                        :key="exercise.id"
-                        :exercise="exercise"
-                        :deleteExercise="deleteExercise"
-                    />
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</template>
-
-<script setup>
-import { onMounted, reactive, ref, computed } from "vue";
-import axiosClient from "../../config/axios";
-import Spinner from "../../components/utils/Spinner.vue";
-import ExerciseCard from "../../components/ExerciseCard.vue";
-import {addExercisePopup} from "../../functions/alerts"
-
-const loading = ref(true);
-var exercises = reactive([]);
-
-function addExercise() {
-    addExercisePopup().then(({exercise}) => {
-        exercises.push(exercise)
-        updateGroupedExercises()
-    })
-}
-
-async function deleteExercise(id) {
+  </template>
+  
+  <script setup>
+  import { onMounted, reactive, ref } from "vue";
+  import axiosClient from "../../config/axios";
+  import Spinner from "../../components/utils/Spinner.vue";
+  import ExerciseCard from "../../components/ExerciseCard.vue";
+  import { addExercisePopup } from "../../functions/alerts";
+  
+  const loading = ref(true);
+  var exercises = reactive([]);
+  
+  function addExercise() {
+    addExercisePopup().then(({ exercise }) => {
+      exercises.push(exercise);
+      updateGroupedExercises();
+    });
+  }
+  
+  async function deleteExercise(id) {
     try {
-        deleteExerciseFromArray(id)
-        updateGroupedExercises()
-
-        await axiosClient.delete("exercises/" + id)
+      deleteExerciseFromArray(id);
+      updateGroupedExercises();
+  
+      await axiosClient.delete("exercises/" + id);
     } catch (e) {
-        console.log(e)
+      console.log(e);
     }
-}
-
-async function getExercises() {
+  }
+  
+  async function getExercises() {
     loading.value = true;
     try {
-        const response = await axiosClient(
-            "exercises/get-all-user/" + localStorage.getItem("id")
-        );
-
-        exercises = response.data;
-
-        updateGroupedExercises()
-
-        loading.value = false;
+      const response = await axiosClient(
+        "exercises/get-all-user/" + localStorage.getItem("id")
+      );
+  
+      exercises = response.data;
+  
+      updateGroupedExercises();
+  
+      loading.value = false;
     } catch (e) {
-        console.log(e);
-        loading.value = false;
+      console.log(e);
+      loading.value = false;
     }
-}
-
-function deleteExerciseFromArray(id) {
-    const index = exercises.findIndex(exercise => exercise.id === id);
-
+  }
+  
+  function deleteExerciseFromArray(id) {
+    const index = exercises.findIndex((exercise) => exercise.id === id);
+  
     if (index !== -1) {
-        exercises.splice(index, 1);
+      exercises.splice(index, 1);
     }
-}
-
-//Group exercises
-const groupedExercises = ref([]);
-
-function updateGroupedExercises() {
+  }
+  
+  //Group exercises
+  const groupedExercises = ref([]);
+  
+  function updateGroupedExercises() {
     groupedExercises.value = exercises.reduce((acc, exercise) => {
-        var groupKey = "No Type";
-
-        switch (parseInt(exercise.muscular_group)) {
-            case 1:
-                groupKey = "Push";
-                break;
-            case 2:
-                groupKey = "Pull";
-                break;
-            case 3:
-                groupKey = "Leg";
-                break;
-            case 4:
-                groupKey = "Core";
-                break;
-        }
-
-        if (!acc[groupKey]) {
-            acc[groupKey] = [];
-        }
-
-        acc[groupKey].push(exercise);
-
-        return acc;
+      var groupKey = "No Type";
+  
+      switch (parseInt(exercise.muscular_group)) {
+        case 1:
+          groupKey = "Push";
+          break;
+        case 2:
+          groupKey = "Pull";
+          break;
+        case 3:
+          groupKey = "Leg";
+          break;
+        case 4:
+          groupKey = "Core";
+          break;
+      }
+  
+      if (!acc[groupKey]) {
+        acc[groupKey] = [];
+      }
+  
+      acc[groupKey].push(exercise);
+  
+      return acc;
     }, {});
-}
-
-onMounted(() => {
+  }
+  
+  function getTooltipContent(group) {
+    const tooltips = {
+      Push: "Excercises that involves: chest, shoulders and triceps",
+      Pull: "Exercises that involves: dorsal, biceps and shoulders",
+      Leg: "Exercises that envolves: ",
+      Core: "Exercises that envolves: ",
+      "No Type": "Comentario genérico",
+    };
+    return tooltips[group];
+  }
+  
+  onMounted(() => {
     getExercises();
-});
-
-</script>
-
-<style scoped>
-.exercises-grid {
+  });
+  </script>
+  
+  <style scoped>
+  .exercises-grid {
     display: grid;
     gap: 1rem;
     grid-template-columns: repeat(2, 1fr);
-}
-</style>
+  }
+  </style>
+  
